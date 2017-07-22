614c8cbef4b7683e2620df618f166322d87efc41
CodeDiablos <codediablos@gmail.com>
2017-07-22 14:12:38 +0800

    Initial commit

---
+++ app/src/main/java/com/cpeat/cpeat/ChooseFragment.java
@@ -0,0 +1,24 @@
+package com.cpeat.cpeat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CodeDiablos on 2017/7/22.
+ */
+
+public class ChooseFragment extends Fragment {
+
+    static final String TAG = "ChooseFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_choose, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/cpeat/cpeat/MainActivityFragment.kt
@@ -0,0 +1,40 @@
+package com.cpeat.cpeat
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    val TAG = "MainActivityFragment"
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.fragment_main, container, false)
+        var editPrice = view.findViewById(R.id.edit_price) as EditText
+        var buttonStart = view.findViewById(R.id.btn_start) as Button
+        buttonStart.setOnClickListener { view ->
+            val newFragment = ChooseFragment()
+            val bundle = Bundle()
+            bundle.putDouble("price", editPrice.text.toString().toDouble())
+            newFragment.arguments = bundle
+
+            var manager = this.activity.supportFragmentManager
+            var transaction = manager.beginTransaction()
+            transaction.replace(R.id.fragment_context, newFragment)
+            transaction.addToBackStack(null);
+            transaction.commit()
+        }
+        return view
+    }
+
+}
